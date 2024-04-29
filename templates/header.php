0b934dffceecc6da4e7f755d4a5e776dfd515821
sathurshanath <sathurshanath2021@gmail.com>
2024-04-29 12:38:22 +0530

    minor change

--- templates/header.php
+++ templates/header.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Schedule me</title>
+  <title>Feedback System</title>
   <!-- BootStrap Link Cdn -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
@@ -32,7 +32,7 @@
     <!-- Custom styles for this template-->
     <link href="templates/sb-admin-2.min.css" rel="stylesheet">
 
-    
+
   </head>
 
 <body>
