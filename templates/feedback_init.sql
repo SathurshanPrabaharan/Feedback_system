475fd657f9cc34a744c4a45aecb15536eea9969e
darship19 <darshika197@gmail.com>
2024-04-29 09:33:31 +0530

    commit_msg

---
+++ templates/feedback_init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Lecture (
+    lecture_id INT PRIMARY KEY,
+    lecture_name VARCHAR(255),
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    username VARCHAR(50),
+    course VARCHAR(100),
+    department VARCHAR(100),
+    outlook_address VARCHAR(255)
+);
